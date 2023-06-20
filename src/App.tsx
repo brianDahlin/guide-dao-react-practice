5df54d438f6f19cbf34f50fc2e974fd60360777a
brianDahlin <bonfun55@gmail.com>
2023-06-20 20:54:22 +0300

    HashRouter added

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { Button } from './components/Button';
 import { Container } from './components/Container';
 import { Input } from './components/Input';
 import { Link } from './components/Link';
+import {HashRouter, Route} from "react-router-dom"
 
 
 
 export default function App () {
  return (
+   <HashRouter>
     <Container>
     <Button label="Кнопка"/>
     <Input  placeholder="Инпут" />
     <Link href="https://royalfools.lol/" label="Royal Fools"/>
     </Container>
+    </HashRouter>
  )
 }
 
